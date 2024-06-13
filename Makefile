62f851e8639c675d4f3b08b0def0484c1d387e2f
Max Gohren <maxgohren@gmail.com>
2024-06-13 18:13:56 -0400

    added boilerplate cairo/x11 display project

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 make:
-	gcc -o cairosimple main.c $(pkg-config --cflags --libs cairo pango pangocairo) -lm
+	gcc -o cannon cairoCannon.c -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/harfbuzz -I/usr/include/cairo -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lharfbuzz -lcairo -lm
+	./cannon
+
+open:
+	gcc -o cannon cairoCannon.c -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/harfbuzz -I/usr/include/cairo -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lharfbuzz -lcairo -lm
+	./cannon
+	xdg-open stroke.png
 
 run:
-	gcc -o cairosimple main.c -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/harfbuzz -I/usr/include/cairo -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lharfbuzz -lcairo -lm
-	./cairosimple test
-	xdg-open test
-
-clean: 
-	if [ -f "test" ]; then rm test; fi
-
+	gcc -o cannon cairoCannon.c -lcairo -lX11
+	./cannon
--- cairoCannon.c
+++ cairoCannon.c
@@ -1,26 +1,99 @@
-#include <cairo.h>
+#include <cairo/cairo.h>
+#include <cairo/cairo-xlib.h>
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // For usleep
 
-int
-main (int argc, char *argv[])
-{
-        cairo_surface_t *surface;
-        cairo_t *cr;
+int main() {
+    Display *display;
+    Window root;
+    Window window;
+    XEvent event;
+    int screen;
 
-        surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 120, 120);
-        cr = cairo_create (surface);
-	/* Examples are in 1.0 x 1.0 coordinate space */
-	cairo_scale (cr, 120, 120);
+    // Open connection to the X server
+    display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        fprintf(stderr, "Cannot open display\n");
+        exit(1);
+    }
 
-	/* Drawing code goes here */
-	cairo_set_line_width (cr, 0.1);
-	cairo_set_source_rgb (cr, 0, 0, 0);
-	cairo_rectangle (cr, 0.25, 0.25, 0.5, 0.5);
-	cairo_stroke (cr);
+    screen = DefaultScreen(display);
+    root = RootWindow(display, screen);
 
-	/* Write output and clean up */
-        cairo_surface_write_to_png (surface, "stroke.png");
-        cairo_destroy (cr);
-        cairo_surface_destroy (surface);
+    // Create a simple window
+    window = XCreateSimpleWindow(display, root, 10, 10, 800, 600, 1,
+                                 BlackPixel(display, screen), WhitePixel(display, screen));
 
-        return 0;
+    // Select input events
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
+
+    // Map (show) the window
+    XMapWindow(display, window);
+
+    // Create a Cairo surface and context for the Xlib window
+    cairo_surface_t *surface = cairo_xlib_surface_create(display, window,
+                                                         DefaultVisual(display, screen), 800, 600);
+    cairo_t *cr = cairo_create(surface);
+
+    int x = 0; // Initial x position of the rectangle
+    int y = 100; // y position of the rectangle
+    int width = 200; // Width of the rectangle
+    int height = 200; // Height of the rectangle
+
+    // Main event loop
+    while (1) {
+        // Check for events
+        while (XPending(display)) {
+            XNextEvent(display, &event);
+
+            if (event.type == KeyPress) {
+                // Exit the loop on key press
+                goto cleanup;
+            }
+        }
+
+        // Clear the background
+        cairo_set_source_rgb(cr, 1, 1, 1); // White
+        cairo_paint(cr);
+
+        // Draw the red rectangle
+        cairo_set_source_rgb(cr, 1, 0, 0); // Red
+        cairo_rectangle(cr, x, y, width, height);
+        cairo_fill(cr);
+
+        // Draw some text
+        cairo_set_source_rgb(cr, 0, 0, 0); // Black
+        cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+        cairo_set_font_size(cr, 40);
+        cairo_move_to(cr, 150, 150);
+        cairo_show_text(cr, "Hello, Cairo!");
+
+        // Flush the drawing operations
+        cairo_surface_flush(surface);
+        XFlush(display);
+
+        // Update the position of the rectangle
+        x += 1; // Move the rectangle to the right
+		width += 1;
+		if (height > 10) {
+			height -= 1;
+		}
+        if (x > 800) {
+            x = -width; // Reset the position if it goes off screen
+        }
+
+        // Wait for 0.1 seconds
+        usleep(16667);
+    }
+
+cleanup:
+    // Clean up
+    cairo_destroy(cr);
+    cairo_surface_destroy(surface);
+    XCloseDisplay(display);
+
+    return 0;
 }
+
