d799a1315d0cd847811508ef7a3d1e1822251e28
Max Gohren <maxgohren@gmail.com>
2024-06-14 15:28:45 -0400

    Added functioning cannon and ball physics display

--- Makefile
+++ Makefile
@@ -1,12 +1,3 @@
 make:
-	gcc -o cannon cairoCannon.c -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/harfbuzz -I/usr/include/cairo -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lharfbuzz -lcairo -lm
-	./cannon
-
-open:
-	gcc -o cannon cairoCannon.c -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/harfbuzz -I/usr/include/cairo -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lharfbuzz -lcairo -lm
-	./cannon
-	xdg-open stroke.png
-
-run:
-	gcc -o cannon cairoCannon.c -lcairo -lX11
+	gcc -o cannon cairoCannon.c `pkg-config --cflags --libs cairo cairo-svg` -lX11 -lm
 	./cannon
--- cairoCannon.c
+++ cairoCannon.c
@@ -1,9 +1,41 @@
 #include <cairo/cairo.h>
+#include <cairo-svg.h>
 #include <cairo/cairo-xlib.h>
 #include <X11/Xlib.h>
+#include <X11/keysym.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> // For usleep
+#include <math.h>
+
+#define FRAMERATE 20
+const float frame_duration = 1000000 / FRAMERATE;
+
+double gravity = 9.81;
+
+struct Ball {	
+	double x;
+	double y;
+	double prevX;
+	double prevY;
+	double vx;
+	double vy;
+	double initialVelocity;
+	double angle; 
+	int collision;
+};
+
+struct Ball ball = {
+					0,  // x
+					600,  // y
+					0,  // prevX
+					0,  // prevY
+					0,  // x velocity 
+					0,  // y velocity
+					20,  // init. velocity m/s
+					75, // angle in degrees
+					0};
+
 
 int main() {
     Display *display;
@@ -37,55 +69,83 @@ int main() {
                                                          DefaultVisual(display, screen), 800, 600);
     cairo_t *cr = cairo_create(surface);
 
-    int x = 0; // Initial x position of the rectangle
-    int y = 100; // y position of the rectangle
-    int width = 200; // Width of the rectangle
-    int height = 200; // Height of the rectangle
+	// calculate ball velocities
+	ball.vx = ball.initialVelocity * cos(ball.angle * M_PI / 180);
+	ball.vy = -(ball.initialVelocity * sin(ball.angle * M_PI / 180));
 
     // Main event loop
     while (1) {
         // Check for events
+pending:
         while (XPending(display)) {
             XNextEvent(display, &event);
 
             if (event.type == KeyPress) {
-                // Exit the loop on key press
-                goto cleanup;
-            }
-        }
+				KeySym key = XLookupKeysym(&event.xkey,0);
+				if(key != XK_space) {
+					// exit
+				   	goto cleanup; 
+				} else {
+					// reset
+					ball.x = 0;
+					ball.y = 600;
+					ball.vx = ball.initialVelocity * cos(ball.angle * M_PI / 180);
+					ball.vy = -(ball.initialVelocity * sin(ball.angle * M_PI / 180));
+					continue;
+            	}
+        	}
+		}
+		// stop rolling
+		if (ball.y == 600 && ball.x != 0 ) { ball.vx = 0; goto pending; } 
 
         // Clear the background
         cairo_set_source_rgb(cr, 1, 1, 1); // White
         cairo_paint(cr);
 
-        // Draw the red rectangle
+        // Draw the red circle
         cairo_set_source_rgb(cr, 1, 0, 0); // Red
-        cairo_rectangle(cr, x, y, width, height);
+        cairo_arc(cr, ball.x, ball.y, 5, 0, 2 * 3.14159);
         cairo_fill(cr);
 
-        // Draw some text
-        cairo_set_source_rgb(cr, 0, 0, 0); // Black
-        cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-        cairo_set_font_size(cr, 40);
-        cairo_move_to(cr, 150, 150);
-        cairo_show_text(cr, "Hello, Cairo!");
+		// wait for 1/60th of a second
+        usleep(frame_duration);
+
+		// print information to the screen about ball
+		cairo_set_source_rgb(cr, 0, 0, 0);
+		cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+		cairo_set_font_size(cr, 20);
+		char posText[100];
+		char velText[100];
+		char grvText[100];
+		snprintf( posText, sizeof(posText), "Ball Position: (%.2f,     %.2f    )", ball.x, 600 - ball.y );
+		snprintf( velText, sizeof(velText), "Ball Velocity: (%.2f m/s, %.2f m/s)", ball.vx, - ball.vy );
+		snprintf( grvText, sizeof(grvText), "Gravity: %.2f m/s^2", gravity);
+		cairo_move_to(cr, 400, 100);
+		cairo_show_text(cr,  posText);
+		cairo_move_to(cr, 400, 125);
+		cairo_show_text(cr,  velText);
+		cairo_move_to(cr, 400, 150);
+		cairo_show_text(cr,  grvText);
 
         // Flush the drawing operations
         cairo_surface_flush(surface);
         XFlush(display);
 
-        // Update the position of the rectangle
-        x += 1; // Move the rectangle to the right
-		width += 1;
-		if (height > 10) {
-			height -= 1;
-		}
-        if (x > 800) {
-            x = -width; // Reset the position if it goes off screen
-        }
+		// gravity and other forces
+		//ball.vy += gravity / (FRAMERATE * FRAMERATE);
+		ball.vy += gravity / 10;
 
-        // Wait for 0.1 seconds
-        usleep(16667);
+        // Update the position of the ball 
+		ball.x += ball.vx;
+		ball.y += ball.vy;
+		
+		// bounds
+        if (ball.x > 800 ) { ball.x = 800; }
+        if (ball.x < 0   ) { ball.x = 0;   }
+		if (ball.y > 600 ) { ball.y = 600; }
+		if (ball.y < 0   ) { ball.y = 0;   }
+
+
     }
 
 cleanup:
